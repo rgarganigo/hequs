6e0ea0786d979723caef5e4ef4198314605fd2c1
phaerya <joaquim.morais-faria@outlook.com>
2024-11-07 14:47:07 +0100

    Commit encore
    accès bdd attention les pirateurs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -21,7 +21,7 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? 'dark'].tint,
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
