a97affa8c640cccde3c8089860a92a7741fe624d
phaerya <joaquim.morais-faria@outlook.com>
2024-12-20 19:24:12 +0100

    Commit ajout règles tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,22 @@
-import { Link, Href } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-
-import { View, Image, SafeAreaView, ScrollView } from 'react-native';
+import { View, Image, SafeAreaView, ScrollView, Text } from 'react-native';
 
 import 'react-native-reanimated';
-import "../global.css"
+import '../global.css';
 
 import { useColorScheme } from '@/components/useColorScheme';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { images } from '@/constants';
+import CustomButton from '@/components/CustomButton';
 
 function App() {
   const colorScheme = useColorScheme();
+  const theme = colorScheme === 'dark' ? DarkTheme : DefaultTheme;
+
   console.log('Current color scheme:', colorScheme); // Log the current theme
 
   return (
-
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <SafeAreaView>
+    <ThemeProvider value={theme}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.background }}>
         <ScrollView contentContainerStyle={{ height: '100%' }}>
           <View className="w-full justify-center items-center max-h-full px-4">
             <Image
@@ -25,7 +24,29 @@ function App() {
               className="w-[130px] h-[84px]"
               resizeMode="contain"
             />
+
+            <Image
+              source={images.cards}
+              className="max-w-[380px] w-full h-[300px]"
+              resizeMode='contain'
+            />
+
+            <View className="relative mt-5">
+              <Text className="text-3xl text-white font-bold">Welcome to{' '}
+                <Text className="text-secondary-200">Hequuuus</Text></Text>
+
+              <Image
+                source={images.path}
+                className="w-[175px] h-[18px] absolute -bottom-4 -right-8"
+                resizeMode='contain'
+              />
+            </View>
+            <Text className="text-sm font-pregular 
+            text-gray-100 mt-7 text-center">Because I LOOOOVE horse jumping
+            </Text>
+
           </View>
+            <CustomButton/>
         </ScrollView>
       </SafeAreaView>
     </ThemeProvider>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
+      colors: {
+        primary: "#161622",
+        secondary: {
+          DEFAULT: "#FF9C01",
+          100: "#FF9001",
+          200: "#FF8E01",
+        },
+        black: {
+          DEFAULT: "#000",
+          100: "#1E1E2D",
+          200: "#232533",
+        },
+        gray: {
+          100: "#CDCDE0",
+        },
+      },
       fontFamily: {
         pthin: ["Poppins-Thin", "sans-serif"],
         pextralight: ["Poppins-ExtraLight", "sans-serif"],
@@ -18,5 +37,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
-
+};
